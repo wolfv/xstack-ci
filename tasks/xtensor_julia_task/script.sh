b9e274fe6fcb48d65c5086a63dfd9b0b8f3267f7
Any One <quantfox@quantstack.com>
2018-12-05 13:27:02 +0100

    ..

--- tasks/xtensor_julia_task/script.sh
+++ tasks/xtensor_julia_task/script.sh
@@ -4,24 +4,10 @@ set -e
 export WORKDIR=`pwd`
 
 apt update
-apt install cmake g++ wget git bzip2 -y
+apt install cmake g++ git -y
 
 export CC=gcc
 export CXX=g++
-
-export MINICONDA_VERSION="latest"
-export MINICONDA_LINUX="Linux-x86_64"
-export MINICONDA_OSX="MacOSX-x86_64"
-
-wget "http://repo.continuum.io/miniconda/Miniconda3-$MINICONDA_VERSION-$MINICONDA_LINUX.sh" -O miniconda.sh;
-bash miniconda.sh -b -u -p $WORKDIR/miniconda
-export PATH="$WORKDIR/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda install nlohmann_json pybind11 numpy pytest -c QuantStack -c conda-forge
-
-export PY_EXE=`which python3`
 
 # install xtl
 
@@ -45,11 +31,15 @@ cd $WORKDIR
 # test xtensor-julia
 
 julia -E "using Pkg; Pkg.add(PackageSpec(name=\"CxxWrap\", version=\"0.8.1\"))"
+echo "CxxWrap Installed"
 export JlCxx_DIR=$(julia -E "using CxxWrap; joinpath(dirname(pathof(CxxWrap)), \"..\", \"deps\", \"usr\", \"lib\", \"cmake\", \"JlCxx\")")
-export JlCxx_DIR=${JlCxx_DIR//\"/}
+
+# removing double quotes with JlCxx_DIR=${JlCxx_DIR//\"/} doesn't work with
+# that potato-shell in debian, so using `tr` here
+export JlCxx_DIR=`echo $JlCxx_DIR | tr -d '"'`
 
 cd xtensor-julia
 mkdir build
 cd build
-cmake .. -DDOWNLOAD_GTEST=ON -DJlCxx_DIR=$JlCxx_DIR -DCMAKE_INSTALL_PREFIX=$WORKDIR/miniconda -DPYTHON_EXECUTABLE=$PY_EXE
+cmake .. -DDOWNLOAD_GTEST=ON -DJlCxx_DIR=$JlCxx_DIR
 make xtest -j16
