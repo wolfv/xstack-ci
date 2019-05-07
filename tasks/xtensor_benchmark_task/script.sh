7916215029fb7c2eafc57be272aaa5e962a61b48
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-05-07 16:19:41 +0200

    add pw

--- tasks/xtensor_benchmark_task/script.sh
+++ tasks/xtensor_benchmark_task/script.sh
@@ -8,17 +8,6 @@ apt install python3-dev python3-pip -y
 
 pip3 install python-openstackclient
 
-
-export OS_PASSWORD=$SECRET_KEY
-
-echo "Secret key 1?"
-echo $SECRET_KEY
-echo $SCK1
-echo "Secret key 2?"
-echo $SECRET_KEY2
-echo $SCK2
-
 echo $OS_PASSWORD
-
 
 openstack server list
