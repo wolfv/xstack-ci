3b87254cb4e4f5976cc9d542d4ed1f8ff65ba1c3
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-05-07 14:27:14 +0200

    add benchmark tests

---
+++ tasks/xtensor_benchmark_task/script.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+export WORKDIR=`pwd`
+
+apt update
+apt install openstack -y
+
+export OS_PASSWORD=$SECRET_KEY
+
+echo "Secret key 1?"
+echo $SECRET_KEY
+echo "Secret key 2?"
+echo $SECRET_KEY2
+
+echo $OS_PASSWORD
+
+
+openstack server list
