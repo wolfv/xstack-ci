d546ab2dccf4fc42700acc69e44c3dd09d217bd1
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-05-07 15:09:48 +0200

    ..

--- tasks/xtensor_benchmark_task/script.sh
+++ tasks/xtensor_benchmark_task/script.sh
@@ -4,7 +4,10 @@ set -e
 export WORKDIR=`pwd`
 
 apt update
-apt install openstack -y
+apt install python3-dev python3-pip -y
+
+pip3 install python-openstackclient
+
 
 export OS_PASSWORD=$SECRET_KEY
 
