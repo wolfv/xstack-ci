b7e0576cfc5bb90869392caa4fb3af53c88ec70c
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-05-07 18:09:02 +0200

    ..

--- tasks/xtensor_benchmark_task/script.sh
+++ tasks/xtensor_benchmark_task/script.sh
@@ -17,9 +17,10 @@ openstack keypair create autokey
 # create openstack server with ubuntu 18.04
 openstack server create benchmakina --flavor b2-7 --image 9f8b2735-4c30-4784-9847-dc18b0e58951 --key-name autokey
 
+openstack server show benchmakina
 openstack server show benchmakina -c addresses -f json > address.json
 
 python3 getip.py
 source getipresult.sh
 
-ssh ubuntu@$SERVER_IPADDR
+openstack server ssh benchmakina --private -4
