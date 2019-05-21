c77eb318a5c1ca9d4c5cce07ca5525caca942d89
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-05-21 15:36:34 +0200

    iterate

--- tasks/xtensor_benchmark_task/script.sh
+++ tasks/xtensor_benchmark_task/script.sh
@@ -5,20 +5,17 @@ echo "Running next version."
 
 export WORKDIR=`pwd`
 
-apt update
-apt install python3-dev python3-pip ssh -y
-
-pip3 install python-openstackclient
-
-printenv | grep OS_
-
 mkdir -p ~/.ssh/
 
 echo $SSH_PRIVATE_KEY > ~/.ssh/id_rsa
 echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
 
-openstack server list
+apt update
+apt install python3-dev python3-pip ssh -y
 
+pip3 install python-openstackclient
+
+openstack server list
 openstack keypair create ngkey --private-key ~/.ssh/id_rsa
 
 # create openstack server with ubuntu 18.04
