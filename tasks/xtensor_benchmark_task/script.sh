fff9b13fc9a468b62fb00bccc6ce40f7e32665ab
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-05-21 15:20:30 +0200

    iterate on benchmarking

--- tasks/xtensor_benchmark_task/script.sh
+++ tasks/xtensor_benchmark_task/script.sh
@@ -10,12 +10,17 @@ pip3 install python-openstackclient
 
 printenv | grep OS_
 
+echo $SSH_PRIVATE_KEY > ~/.ssh/id_rsa
+echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
+
 openstack server list
 
-openstack keypair create autokey
+head -n1 ~/.ssh/id_rsa
+
+openstack keypair create ngkey --private-key ~/.ssh/id_rsa
 
 # create openstack server with ubuntu 18.04
-openstack server create benchmakina --flavor b2-7 --image 9f8b2735-4c30-4784-9847-dc18b0e58951 --key-name autokey
+openstack server create benchmakina --flavor b2-7 --image 9f8b2735-4c30-4784-9847-dc18b0e58951 --key-name ngkey
 
 openstack server show benchmakina
 openstack server show benchmakina -c addresses -f json > address.json
@@ -23,4 +28,7 @@ openstack server show benchmakina -c addresses -f json > address.json
 python3 getip.py
 source getipresult.sh
 
-openstack server ssh benchmakina --private -4
+echo "IP ADDRESS "
+echo $SERVER_IPADDR
+
+# openstack server ssh benchmakina --private -4
