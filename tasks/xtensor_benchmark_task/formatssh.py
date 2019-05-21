ea1eb512cbf7fb3fc6e5add149efffee248f1d28
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-05-21 18:14:19 +0200

    ..

---
+++ tasks/xtensor_benchmark_task/formatssh.py
@@ -0,0 +1,9 @@
+import os
+
+private_key = os.environ['SSH_PRIVATE_KEY']
+public_key = os.environ['SSH_PUBLIC_KEY']
+
+private_key.replace(' ', '\n')
+
+with open('~/ssh_key', 'w') as fo:
+	fo.write(private_key)
