632508890a3473ab0c161b473d97138b755d65b5
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-05-27 11:05:31 +0200

    new benchmarking

---
+++ tasks/xtensor_benchmark_task/bench_script.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+set -e
+
+cd ~
+export WORKDIR=`pwd`
+
+sudo apt-get update
+sudo apt-get install cmake g++ wget git ninja -y
+
+export CC=gcc
+export CXX=g++
+
+# CONDA INSTALLATION
+
+export MINICONDA_VERSION="latest"
+export MINICONDA_LINUX="Linux-x86_64"
+export MINICONDA_OSX="MacOSX-x86_64"
+
+wget "http://repo.continuum.io/miniconda/Miniconda3-$MINICONDA_VERSION-$MINICONDA_LINUX.sh" -O miniconda.sh;
+bash miniconda.sh -b -u -p $WORKDIR/miniconda
+export PATH="$WORKDIR/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda install pybind11 asv -c conda-forge
+
+# SOME SYSTEM FIDDLING
+
+sudo mount -t /dev/sdb /benchresults
+
+mkdir -p ~/.ssh/
+
+cat >> ~/.ssh/config <<EOL
+Host gitkey.com
+    Hostname github.com
+    User git
+    IdentityFile /benchresults/git_key
+EOL
+
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+
+# RUN THE BENCHMARKS
+
+cd /benchresults
+
+if [ ! -d "xtensor-asv" ]; then
+    git clone git@gitkey.com:wolfv/xtensor-asv
+fi
+
+cd /benchresults/xtensor-asv
+
+git pull origin master
+
+asv run
+
+asv gh-pages
--- tasks/xtensor_benchmark_task/script.sh
+++ tasks/xtensor_benchmark_task/script.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -v
 
-echo "Running next version."
-
 export WORKDIR=`pwd`
 
 apt-get update
@@ -13,36 +11,41 @@ chmod 600 ssh_key
 
 pip3 install python-openstackclient
 
-openstack server list
+export SERVER_NAME="benchmachine"
 
 # create openstack server with ubuntu 18.04
-openstack server create benchmakina --flavor b2-7 --image 9f8b2735-4c30-4784-9847-dc18b0e58951 --key-name ngkey
+openstack server create $SERVER_NAME --flavor b2-7 --image 9f8b2735-4c30-4784-9847-dc18b0e58951 --key-name ngkey
 
+# wait until the openstack server is active!
 active="FALSE"
-
 until [ "$active" = "ACTIVE" ]
 do
-  active=$(openstack server show benchmakina -f json | jq -r .status)
+  active=$(openstack server show $SERVER_NAME -f json | jq -r .status)
   echo "Status: " $active
 done
 
-openstack server show benchmakina
-openstack server show benchmakina -c addresses -f json > address.json
+openstack server add volume $SERVER_NAME benchresults
 
-cat address.json
+openstack server show $SERVER_NAME -c addresses -f json > address.json
 
+# extract the IP address.
 export SERVER_IPADDR=$(python3 $WORKDIR/buildscripts/tasks/xtensor_benchmark_task/getip.py)
 
-echo "IP ADDRESS ", $SERVER_IPADDR
+mkdir -p ~/.ssh
+ssh-keyscan $SERVER_IPADDR >> ~/.ssh/known_hosts
 
-ssh -o StrictHostKeyChecking=no -i ssh_key ubuntu@$SERVER_IPADDR "/bin/uname -a"
-ssh -o StrictHostKeyChecking=no -i ssh_key ubuntu@$SERVER_IPADDR "sudo apt-get update"
-ssh -o StrictHostKeyChecking=no -i ssh_key ubuntu@$SERVER_IPADDR "sudo apt-get install cmake git g++ -y"
-ssh -o StrictHostKeyChecking=no -i ssh_key ubuntu@$SERVER_IPADDR "git clone https://github.com/QuantStack/xtl"
-ssh -o StrictHostKeyChecking=no -i ssh_key ubuntu@$SERVER_IPADDR "git clone https://github.com/QuantStack/xtensor"
-ssh -o StrictHostKeyChecking=no -i ssh_key ubuntu@$SERVER_IPADDR "git clone https://github.com/QuantStack/xsimd"
-ssh -o StrictHostKeyChecking=no -i ssh_key ubuntu@$SERVER_IPADDR "cd xtl && mkdir build && cd build && cmake .. && sudo make install"
-ssh -o StrictHostKeyChecking=no -i ssh_key ubuntu@$SERVER_IPADDR "cd xsimd && mkdir build && cd build && cmake .. && sudo make install"
-ssh -o StrictHostKeyChecking=no -i ssh_key ubuntu@$SERVER_IPADDR "cd xtensor && mkdir build && cd build && cmake .. -DBUILD_BENCHMARK=ON -DXTENSOR_USE_XSIMD=ON && make xbenchmark"
+scp $WORKDIR/buildscripts/tasks/xtensor_benchmark_task/bench_script.sh ubuntu@$SERVER_IPADDR:~/bench_script.sh
 
-openstack server delete benchmakina
+ssh -i ssh_key ubuntu@$SERVER_IPADDR "sh ~/bench_script.sh"
+
+# ssh -i ssh_key ubuntu@$SERVER_IPADDR "/bin/uname -a"
+# ssh -i ssh_key ubuntu@$SERVER_IPADDR "sudo apt-get update"
+# ssh -i ssh_key ubuntu@$SERVER_IPADDR "sudo apt-get install cmake git g++ -y"
+# ssh -i ssh_key ubuntu@$SERVER_IPADDR "git clone https://github.com/QuantStack/xtl"
+# ssh -i ssh_key ubuntu@$SERVER_IPADDR "git clone https://github.com/QuantStack/xtensor"
+# ssh -i ssh_key ubuntu@$SERVER_IPADDR "git clone https://github.com/QuantStack/xsimd"
+# ssh -i ssh_key ubuntu@$SERVER_IPADDR "cd xtl && mkdir build && cd build && cmake .. && sudo make install"
+# ssh -i ssh_key ubuntu@$SERVER_IPADDR "cd xsimd && mkdir build && cd build && cmake .. && sudo make install"
+# ssh -i ssh_key ubuntu@$SERVER_IPADDR "cd xtensor && mkdir build && cd build && cmake .. -DBUILD_BENCHMARK=ON -DXTENSOR_USE_XSIMD=ON && make xbenchmark"
+
+openstack server delete $SERVER_NAME
