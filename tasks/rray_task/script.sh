080665dd753c6ee67557c660d79fe48369ee8765
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-06-04 18:08:40 +0200

    use R from ubuntu

--- tasks/rray_task/script.sh
+++ tasks/rray_task/script.sh
@@ -4,22 +4,22 @@ set -e
 export WORKDIR=`pwd`
 
 apt-get update
-apt-get install cmake g++ wget git gzip -y
+apt-get install cmake g++ wget git gzip r-base-dev r-cran-devtools -y
 
 export CC=gcc
 export CXX=g++
 
-export MINICONDA_VERSION="latest"
-export MINICONDA_LINUX="Linux-x86_64"
-export MINICONDA_OSX="MacOSX-x86_64"
+# export MINICONDA_VERSION="latest"
+# export MINICONDA_LINUX="Linux-x86_64"
+# export MINICONDA_OSX="MacOSX-x86_64"
 
-wget "http://repo.continuum.io/miniconda/Miniconda3-$MINICONDA_VERSION-$MINICONDA_LINUX.sh" -O miniconda.sh;
-bash miniconda.sh -b -u -p $WORKDIR/miniconda
-export PATH="$WORKDIR/miniconda/bin:$PATH"
-hash -r
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda install r r-rcpp r-base r-covr r-devtools ncurses readline -c conda-forge
+# wget "http://repo.continuum.io/miniconda/Miniconda3-$MINICONDA_VERSION-$MINICONDA_LINUX.sh" -O miniconda.sh;
+# bash miniconda.sh -b -u -p $WORKDIR/miniconda
+# export PATH="$WORKDIR/miniconda/bin:$PATH"
+# hash -r
+# conda config --set always_yes yes --set changeps1 no
+# conda update -q conda
+# conda install r r-rcpp r-base r-covr r-devtools ncurses readline -c conda-forge
 
 # install xtl
 cd xtl
@@ -34,5 +34,7 @@ cd xtensor
 mkdir build
 cd build
 
+Rscript -e 'install.packages("devtools", repos="http://cran.us.r-project.org")'
+Rscript -e 'install.packages("covr", repos="http://cran.us.r-project.org")'
 Rscript -e 'remotes::install_github("DavisVaughan/Xtensor.R", ref = "dev", force = TRUE)'
 Rscript -e 'covr::codecov()'
